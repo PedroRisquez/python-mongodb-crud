dcd6faa84c55bcff7fb382a4257e519693fda71b
Pedro Rísquez Calvillo <pedrorisquez@hotmail.com>
2024-06-12 09:09:01 +0200

    space typo

--- schemas/user.py
+++ schemas/user.py
@@ -7,4 +7,4 @@ def userEntity(item) -> dict:
             }
 
 def usersEntity(entity) -> list:
-    return [userEntity (item)  for item in entity]
+    return [userEntity (item) for item in entity]
